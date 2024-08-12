3a5beb9108bf1989740ad52d2773d9a1767f47c3
jisu217 <164330581+jisu217@users.noreply.github.com>
2024-08-12 10:17:46 +0900

    Add files via upload

---
+++ practice/replace.py
@@ -0,0 +1,7 @@
+string = 'abcdfe2a354a32a'
+string = string.replace('a', 'A')
+print(string) #Abcdfe2A354A32A
+
+# >> string = 'abcd'
+# >> string.replace('b', 'B') 반환된 값을 string 변수에 다시 할당하지 않음 / 파이썬 문자열 immutable
+# >> print(string)
