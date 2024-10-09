6c4f2ca51c8d4ec79db71e53e194c590d115244f
jisu217 <164330581+jisu217@users.noreply.github.com>
2024-10-09 14:09:49 +0900

    Delete Software_coding [A]_code directory

--- Software_coding [A]_code/2.개념/while반복문.py
+++
@@ -1,8 +0,0 @@
-answer = input('왕이 넘어지면? ')
-
-while answer != '킹콩':
-    print('정답이 아닙니다.')
-    answer = input('왕이 넘어지면? ')
-
-print('정답입니다!')
-
--- Software_coding [A]_code/2.개념/라이브러리.py
+++
@@ -1,5 +0,0 @@
-import time # time 모듈_시간 관련 함수들을 사용할 수 있음
-
-print(time.ctime()) # 현재 시간_ "Mon Sep 5 14:55:02 2024"와 같은 형식
-time.sleep(10) # 프로그램 실행 10초 일시 정지
-print(time.ctime())
