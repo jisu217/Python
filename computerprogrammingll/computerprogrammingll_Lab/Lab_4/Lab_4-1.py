d20044e2168785f268cdd40e00c5dce0d2988188
jisu217 <164330581+jisu217@users.noreply.github.com>
2024-10-09 14:31:12 +0900

    Add files via upload

---
+++ computerprogrammingll/computerprogrammingll_Lab/Lab_4/Lab_4-1.py
@@ -0,0 +1,16 @@
+# 사용자로부터 숫자 5개를 입력받고, 가장 큰 값을 찾아서 출력하는 프로그램
+
+for n in range(5) : #0,1,2,3,4
+
+    # 정수 입력
+    num = int(input("정수입력:"))
+
+    # max값 처음 입력받은 값으로 초기화
+    if n == 0 :
+       maxNum = num
+
+    # max값 갱신
+    if maxNum < num :
+        maxNum = num
+
+print(f"입력받은 가장 큰 정수 = {maxNum}")
